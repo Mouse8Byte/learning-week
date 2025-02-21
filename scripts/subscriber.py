3613093451e3098341dfed080e765383f00dc59a
MouseByte <mouse8byte@gmail.com>
2025-02-21 19:08:25 +0700

    Update subscriber.py

--- scripts/subscriber.py
+++ scripts/subscriber.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
+import sys
+import termios
+import tty
 
-def callback(data):
-    rospy.loginfo(f"Menerima pesan: {data.data}")  # Menampilkan pesan yang diterima
+# Node Input: Mengirim perintah ke topik /turtle_commands
+def get_key():
+    tty.setraw(sys.stdin.fileno())
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(sys.stdin))
+    return key
 
-def listener():
-    rospy.init_node('listener', anonymous=True)  # Inisialisasi node dengan nama 'listener'
-    rospy.Subscriber('/chatter', String, callback)  # Subscribe ke topic 'chatter'
+def input_node():
+    rospy.init_node('input_node', anonymous=True)
+    pub = rospy.Publisher('/turtle_commands', String, queue_size=10)
+    rate = rospy.Rate(10)
+    
+    print("Gunakan WASD untuk menggerakkan TurtleSim. Tekan Q untuk keluar.")
+    while not rospy.is_shutdown():
+        key = get_key()
+        if key in ['w', 'a', 's', 'd']:
+            rospy.loginfo(f"Sending command: {key}")
+            pub.publish(key)
+        elif key == 'q':
+            break
+        rate.sleep()
 
-    rospy.spin()  # Looping agar tetap aktif
-
-if __name__ == "__main__":
-    listener()
+if _name_ == '_main_':
+    try:
+        input_node()
+    except rospy.ROSInterruptException:
+        pass
